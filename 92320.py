3b0fc2ab0c690c71e7749b70159ba27c30e3e040
KevinNieUCSF <Kevin.Nie@ucsf.edu>
2020-09-23 21:50:31 -0700

    latest update

--- 92320.py
+++ 92320.py
@@ -2,6 +2,8 @@
 import sys
 import os
 import Bio.PDB
+import numpy as np
+import math
 try:
     os.chdir('/Users/kevin/KN_2020_Fundamentals/')
 except:
@@ -18,14 +20,28 @@ def read_input():
         except:
             print('Invalid Filename, Please Try Again')
             continue
-
-    return None
+    return structure
+def magnitude(distvec):
+    return math.sqrt(sum(pow(element, 2) for element in distvec))
 
 def calculate_contacts(structure):
     """ For every pair of residues in the structure, find if the residues
     or any of the alternate locations make contact.  If so add them to
     the list of contacts.
     [NOTE: not sure if this should be a list or a dictionary...] """
-    return None
-read_input()
+    clash=-2
+    for atom in structure.get_atoms():
+        for btom in structure.get_atoms():
+            distvec=list(np.array(btom.get_coord()) - np.array(atom.get_coord()))
+            print(magnitude(distvec))
+            print('---')
+            if magnitude(distvec)<=4:
+                clash=clash+1
+                print('Clash Found')
+                print(clash)
+            else:
+                pass
+    return clash
+structure=read_input()
+print("Clash Score: "+str(calculate_contacts(structure)))
 print('All Done')
