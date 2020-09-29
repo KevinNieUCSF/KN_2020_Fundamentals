31cfe537402cb46ecd1c644b2eadcbccfb0f9995
KevinNieUCSF <Kevin.Nie@ucsf.edu>
2020-09-29 10:16:44 -0700

    update tuesday morning, tweaking fetchmol capabilities to accept more

--- funproject.py
+++ funproject.py
@@ -38,6 +38,214 @@ def pdblistfilter(qmainl,iqnmr,iqxray):
         if element in qxrayl:
             qxrayfinal.append(element)
     return qnmrfinal, qxrayfinal
+def molcounternmr(test,nmr):
+    try:
+        if (float(test['polymer']['@weight']))<=2500:
+            nmr[2.5]+=1
+        elif (float(test['polymer']['@weight']))<=5000:
+            nmr[5]+=1
+        elif (float(test['polymer']['@weight']))<=7500:
+            nmr[7.5]+=1
+        elif (float(test['polymer']['@weight']))<=10000:
+            nmr[10]+=1
+        elif (float(test['polymer']['@weight']))<=12500:
+            nmr[12.5]+=1
+        elif (float(test['polymer']['@weight']))<=15000:
+            nmr[15]+=1
+        elif (float(test['polymer']['@weight']))<=17500:
+            nmr[17.5]+=1
+        elif (float(test['polymer']['@weight']))<=20000:
+            nmr[20]+=1
+        elif (float(test['polymer']['@weight']))<=22500:
+            nmr[22.5]+=1
+        elif (float(test['polymer']['@weight']))<=25000:
+            nmr[25]+=1
+        elif (float(test['polymer']['@weight']))<=27500:
+            nmr[27.5]+=1
+        elif (float(test['polymer']['@weight']))<=30000:
+            nmr[30]+=1
+        elif (float(test['polymer']['@weight']))<=32500:
+            nmr[32.5]+=1
+        elif (float(test['polymer']['@weight']))<=35000:
+            nmr[35]+=1
+        elif (float(test['polymer']['@weight']))<=37500:
+            nmr[37.5]+=1
+    except:
+        polymer=test['polymer']
+        mol=0
+        if type(polymer) is list:
+            for poly in polymer:
+                mol+=float(poly['@weight'])
+        if mol<=2500:
+            nmr[2.5]+=1
+        elif mol<=5000:
+            nmr[5]+=1
+        elif mol<=7500:
+            nmr[7.5]+=1
+        elif mol<=10000:
+            nmr[10]+=1
+        elif mol<=12500:
+            nmr[12.5]+=1
+        elif mol<=15000:
+            nmr[15]+=1
+        elif mol<=17500:
+            nmr[17.5]+=1
+        elif mol<=20000:
+            nmr[20]+=1
+        elif mol<=22500:
+            nmr[22.5]+=1
+        elif mol<=25000:
+            nmr[25]+=1
+        elif mol<=27500:
+            nmr[27.5]+=1
+        elif mol<=30000:
+            nmr[30]+=1
+        elif mol<=32500:
+            nmr[32.5]+=1
+        elif mol<=35000:
+            nmr[35]+=1
+        elif mol<=37500:
+            nmr[37.5]+=1
+        pass
+    return nmr
+def molcounterxray(test,xray):
+    try:
+        if (float(test['polymer']['@weight']))<=20000:
+            xray[20]+=1
+        elif (float(test['polymer']['@weight']))<=40000:
+            xray[40]+=1
+        elif (float(test['polymer']['@weight']))<=60000:
+            xray[60]+=1
+        elif (float(test['polymer']['@weight']))<=80000:
+            xray[80]+=1
+        elif (float(test['polymer']['@weight']))<=100000:
+            xray[100]+=1
+        elif (float(test['polymer']['@weight']))<=120000:
+            xray[120]+=1
+        elif (float(test['polymer']['@weight']))<=140000:
+            xray[140]+=1
+        elif (float(test['polymer']['@weight']))<=160000:
+            xray[160]+=1
+        elif (float(test['polymer']['@weight']))<=180000:
+            xray[180]+=1
+        elif (float(test['polymer']['@weight']))<=200000:
+            xray[200]+=1
+        elif (float(test['polymer']['@weight']))<=220000:
+            xray[220]+=1
+        elif (float(test['polymer']['@weight']))<=240000:
+            xray[240]+=1
+        elif (float(test['polymer']['@weight']))<=260000:
+            xray[260]+=1
+        elif (float(test['polymer']['@weight']))<=280000:
+            xray[280]+=1
+        elif (float(test['polymer']['@weight']))<=1000000:
+            xray[1000]+=1
+    except:
+        polymer=test['polymer']
+        mol=0
+        if type(polymer) is list:
+            for poly in polymer:
+                mol+=float(poly['@weight'])
+        if mol<=20000:
+            xray[20]+=1
+        elif mol<=40000:
+            xray[40]+=1
+        elif mol<=60000:
+            xray[60]+=1
+        elif mol<=80000:
+            xray[80]+=1
+        elif mol<=100000:
+            xray[100]+=1
+        elif mol<=120000:
+            xray[120]+=1
+        elif mol<=140000:
+            xray[140]+=1
+        elif mol<=160000:
+            xray[160]+=1
+        elif mol<=180000:
+            xray[180]+=1
+        elif mol<=200000:
+            xray[200]+=1
+        elif mol<=220000:
+            xray[220]+=1
+        elif mol<=240000:
+            xray[240]+=1
+        elif mol<=260000:
+            xray[260]+=1
+        elif mol<=280000:
+            xray[280]+=1
+        elif mol<=1000000:
+            xray[1000]+=1
+    return xray
+def molcounterart(test,art):
+    try:
+        if (float(test['polymer']['@weight']))<=20000:
+            art[20]+=1
+        elif (float(test['polymer']['@weight']))<=40000:
+            art[40]+=1
+        elif (float(test['polymer']['@weight']))<=60000:
+            art[60]+=1
+        elif (float(test['polymer']['@weight']))<=80000:
+            art[80]+=1
+        elif (float(test['polymer']['@weight']))<=100000:
+            art[100]+=1
+        elif (float(test['polymer']['@weight']))<=120000:
+            art[120]+=1
+        elif (float(test['polymer']['@weight']))<=140000:
+            art[140]+=1
+        elif (float(test['polymer']['@weight']))<=160000:
+            art[160]+=1
+        elif (float(test['polymer']['@weight']))<=180000:
+            art[180]+=1
+        elif (float(test['polymer']['@weight']))<=200000:
+            art[200]+=1
+        elif (float(test['polymer']['@weight']))<=220000:
+            art[220]+=1
+        elif (float(test['polymer']['@weight']))<=240000:
+            art[240]+=1
+        elif (float(test['polymer']['@weight']))<=260000:
+            art[260]+=1
+        elif (float(test['polymer']['@weight']))<=280000:
+            art[280]+=1
+        elif (float(test['polymer']['@weight']))<=1000000:
+            art[1000]+=1
+    except:
+        polymer=test['polymer']
+        mol=0
+        if type(polymer) is list:
+            for poly in polymer:
+                mol+=float(poly['@weight'])
+        if mol<=20000:
+            art[20]+=1
+        elif mol<=40000:
+            art[40]+=1
+        elif mol<=60000:
+            art[60]+=1
+        elif mol<=80000:
+            art[80]+=1
+        elif mol<=100000:
+            art[100]+=1
+        elif mol<=120000:
+            art[120]+=1
+        elif mol<=140000:
+            art[140]+=1
+        elif mol<=160000:
+            art[160]+=1
+        elif mol<=180000:
+            art[180]+=1
+        elif mol<=200000:
+            art[200]+=1
+        elif mol<=220000:
+            art[220]+=1
+        elif mol<=240000:
+            art[240]+=1
+        elif mol<=260000:
+            art[260]+=1
+        elif mol<=280000:
+            art[280]+=1
+        elif mol<=1000000:
+            art[1000]+=1
+    return art
 def fetchmol(qnmrfinal,qxrayfinal,qartfinal):
     """gets the molecular weight of each hit for each generated ID list and then generates a count
     for the coming bar graph. mol count info stored in dicts. The info of what weight each ID is is not recorded,
@@ -50,110 +258,10 @@ def fetchmol(qnmrfinal,qxrayfinal,qartfinal):
     art={i:0 for i in xrayartmolrange}
     art[1000]=0
     for hit in qnmrfinal:
-        try:
-            test=pd.get_all_info(hit)
-            if (float(test['polymer']['@weight']))<=2500:
-                nmr[2.5]+=1
-            elif (float(test['polymer']['@weight']))<=5000:
-                nmr[5]+=1
-            elif (float(test['polymer']['@weight']))<=7500:
-                nmr[7.5]+=1
-            elif (float(test['polymer']['@weight']))<=10000:
-                nmr[10]+=1
-            elif (float(test['polymer']['@weight']))<=12500:
-                nmr[12.5]+=1
-            elif (float(test['polymer']['@weight']))<=15000:
-                nmr[15]+=1
-            elif (float(test['polymer']['@weight']))<=17500:
-                nmr[17.5]+=1
-            elif (float(test['polymer']['@weight']))<=20000:
-                nmr[20]+=1
-            elif (float(test['polymer']['@weight']))<=22500:
-                nmr[22.5]+=1
-            elif (float(test['polymer']['@weight']))<=25000:
-                nmr[25]+=1
-            elif (float(test['polymer']['@weight']))<=27500:
-                nmr[27.5]+=1
-            elif (float(test['polymer']['@weight']))<=30000:
-                nmr[30]+=1
-            elif (float(test['polymer']['@weight']))<=32500:
-                nmr[32.5]+=1
-            elif (float(test['polymer']['@weight']))<=35000:
-                nmr[35]+=1
-            elif (float(test['polymer']['@weight']))<=37500:
-                nmr[37.5]+=1
-        except:
-            pass
-    for hit in qxrayfinal:
-        try:
-            test=pd.get_all_info(hit)
-            if (float(test['polymer']['@weight']))<=20000:
-                xray[20]+=1
-            elif (float(test['polymer']['@weight']))<=40000:
-                xray[40]+=1
-            elif (float(test['polymer']['@weight']))<=60000:
-                xray[60]+=1
-            elif (float(test['polymer']['@weight']))<=80000:
-                xray[80]+=1
-            elif (float(test['polymer']['@weight']))<=100000:
-                xray[100]+=1
-            elif (float(test['polymer']['@weight']))<=120000:
-                xray[120]+=1
-            elif (float(test['polymer']['@weight']))<=140000:
-                xray[140]+=1
-            elif (float(test['polymer']['@weight']))<=160000:
-                xray[160]+=1
-            elif (float(test['polymer']['@weight']))<=180000:
-                xray[180]+=1
-            elif (float(test['polymer']['@weight']))<=200000:
-                xray[200]+=1
-            elif (float(test['polymer']['@weight']))<=220000:
-                xray[220]+=1
-            elif (float(test['polymer']['@weight']))<=240000:
-                xray[240]+=1
-            elif (float(test['polymer']['@weight']))<=260000:
-                xray[260]+=1
-            elif (float(test['polymer']['@weight']))<=280000:
-                xray[280]+=1
-            elif (float(test['polymer']['@weight']))<=1000000:
-                xray[1000]+=1
-        except:
-            pass
-    for hit in qartfinal:
-        try:
-            test=pd.get_all_info(hit)
-            if (float(test['polymer']['@weight']))<=20000:
-                art[20]+=1
-            elif (float(test['polymer']['@weight']))<=40000:
-                art[40]+=1
-            elif (float(test['polymer']['@weight']))<=60000:
-                art[60]+=1
-            elif (float(test['polymer']['@weight']))<=80000:
-                art[80]+=1
-            elif (float(test['polymer']['@weight']))<=100000:
-                art[100]+=1
-            elif (float(test['polymer']['@weight']))<=120000:
-                art[120]+=1
-            elif (float(test['polymer']['@weight']))<=140000:
-                art[140]+=1
-            elif (float(test['polymer']['@weight']))<=160000:
-                art[160]+=1
-            elif (float(test['polymer']['@weight']))<=180000:
-                art[180]+=1
-            elif (float(test['polymer']['@weight']))<=200000:
-                art[200]+=1
-            elif (float(test['polymer']['@weight']))<=220000:
-                art[220]+=1
-            elif (float(test['polymer']['@weight']))<=240000:
-                art[240]+=1
-            elif (float(test['polymer']['@weight']))<=260000:
-                art[260]+=1
-            elif (float(test['polymer']['@weight']))<=280000:
-                art[280]+=1
-            elif (float(test['polymer']['@weight']))<=1000000:
-                art[1000]+=1
-        except:
-            pass
+        test=pd.get_all_info(hit)
+        nmr=molcounternmr(test,nmr)
+        xray=molcounterxray(test,xray)
+        art=molcounterart(test,art)
     print("NMR Mol Weight Tally:"+str(nmr))
     print("Xray Mol Weight Tally:"+str(xray))
     print("Artifact Mol Weight Tally:"+str(art))
@@ -182,6 +290,6 @@ art={i:0 for i in xrayartmolrange}
 art[1000]=0
 hit='1pvn'
 test=pd.get_all_info(hit)
-print(test['polymer']['@weight'])
+print(test)
 HAHAHAHAA THIS DOESNT WORK FOR SOME PDB IDS HOOHOOHEEEHEE I LOVE CODING"""
 #print(test['molDescription']['structureId']['polymer']['@weight'])
